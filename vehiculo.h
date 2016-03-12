998aaca119187b43ff7f14f5b5539c2dd25a0cb7
HectorGonBar96 <hgonzb01@estudiantes.unileon.es>
2016-03-12 16:38:21 +0100

    MODIFICACION

--- vehiculo.h
+++ vehiculo.h
@@ -1,11 +1,49 @@
 #ifndef VEHICULO_H
 #define VEHICULO_H
 
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <sstream>
+
+using namespace std;
 
 class vehiculo
 {
 public:
     vehiculo();
-};
 
-#endif // VEHICULO_H
+    //constructor y destructor de la clase
+    vehiculo(string tipo,string nombre,string matricula,int ruedas,string color,string combustible,string accesorio, bool burbuja , bool bocina , bool portavasos,bool cocheDeHomer);
+
+    string tipo;
+    string nombre;
+    string matricula;
+    int ruedas;
+    string color;
+    string combustible;
+    string accesorio;
+    bool burbuja;
+    bool bocina;
+    bool portavasos;
+    bool cocheDeHomer;
+
+
+    string getTipo(string xtipo);
+    void setNombre(string xnombre);
+    void generarMatricula(string xmatricula);
+    void setRuedas(int xruedas);
+    void setColor(string xcolor);
+    void setAccesorio(string xaccesorio);
+    void setCombustible(string xcombustible);
+    void setBurbuja(bool burbuja);
+    void setBocina(bool xbocina);
+    void setPortavasos(bool xportavasos);
+    void setCocheDeHomer(bool xcocheDeHomer);
+
+
+
+
+
+
+};
