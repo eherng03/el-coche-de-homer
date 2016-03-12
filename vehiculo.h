0f19eda8d71270f3160c455c79aaa8e71c5b95e2
HectorGonBar96 <hgonzb01@estudiantes.unileon.es>
2016-03-12 18:31:24 +0100

    GETTERS Y POTENCIA

--- vehiculo.h
+++ vehiculo.h
@@ -19,17 +19,29 @@ public:
 
 
 
-    string getTipo(string xtipo);
+   
     void setNombre(string xnombre);
     string generarMatricula();
-    void setRuedas(string xruedas);     //para no tener que cambiarlo en la combo box
+    void setRuedas(int xruedas);
+    void setPotencia(int xpotencia);//para no tener que cambiarlo en la combo box
     void setColor(string xcolor);
     void setAccesorio(string xaccesorio);
     void setCombustible(string xcombustible);
     void setBurbuja(bool burbuja);
     void setBocina(bool xbocina);
     void setPortavasos(bool xportavasos);
-    void setCocheDeHomer(bool xcocheDeHomer);
+
+    
+    string getTipo();
+    string getNombre();
+    int getRuedas();
+    int getPotencia();
+    string getColor();
+    string getAccesorio();
+    string getCombustible();
+    bool getBurbuja();
+    bool getBocina();
+    bool getPortavasos();
 
 
 private:
