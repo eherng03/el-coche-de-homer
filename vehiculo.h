48f11f1f418d7b719abf4c44820e27021393e166
HectorGonBar96 <hgonzb01@estudiantes.unileon.es>
2016-03-13 20:04:36 +0100

    MODIFICACION

--- vehiculo.h
+++ vehiculo.h
@@ -1,94 +1,54 @@
-#include "vehiculo.h"
 #include <iostream>
-#include <string.h>
 #include <string>
+#include <string.h>
 #include <sstream>
 
 using namespace std;
 
+class vehiculo
+{
+public:
+    vehiculo();
 
-vehiculo::vehiculo(){
+    //constructor y destructor de la clase
+    vehiculo(string tipo,string nombre,string matricula,int ruedas,string potencia,string color,string combustible,string accesorio, bool burbuja , bool bocina , bool portavasos,bool cocheDeHomer);
 
-}
 
-vehiculo::vehiculo(string xtipo, string xnombre, string xmatricula,int xruedas,string xpotencia,string xcolor,string xaccesorio,string xcombustible,bool xbocina,bool xburbuja,bool xportavasos,bool xcocheDeHomer ){
 
-  this->tipo = xtipo;
-  this->nombre = xnombre;
-  this->matricula = xmatricula;
-  this->ruedas = xruedas;
-  this->potencia=xpotencia;
-  this->color = xcolor;
-  this->setAccesorio(xaccesorio);
-  this->combustible = xcombustible;
-  this->bocina = xbocina;
-  this->burbuja = xburbuja;
-  this->portavasos = xportavasos;
+
+    string getTipo();
+    string getMatricula();
+    string getNombre();
+    void setNombre(string xnombre);
+    string generarMatricula();
+    void setRuedas(int xruedas);     //para no tener que cambiarlo en la combo box
+    void setPotencia(string xpotencia);
+    void setColor(string xcolor);
+    void setAccesorio(string xaccesorio);
+    void setCombustible(string xcombustible);
+    void setBurbuja(bool burbuja);
+    void setBocina(bool xbocina);
+    void setPosavasos(bool xposavasos);
+    void setCocheDeHomer(bool xcocheDeHomer);
+    string toString();
+
+private:
+
+    string tipo;
+    string nombre;
+    string matricula;
+    int ruedas;
+    string potencia;
+    string color;
+    string combustible;
+    bool kitEmergencia;
+    bool ruedaRepuesto;
+    bool burbuja;
+    bool bocina;
+    bool portavasos;
+    bool cocheDeHomer;
+
+
 
 };
-
-string vehiculo::getTipo(){
-    return this->tipo;
-}
-
-string vehiculo::getNombre(){
-    return this->nombre;
-}
-
-string vehiculo::getMatricula(){
-    return this->matricula;
-}
-
-
-void vehiculo::setNombre(string xnombre){
-    this->nombre = xnombre;
-}
-
-string vehiculo::generarMatricula(){
-
-}
-
-void vehiculo::setRuedas(int xruedas){
-    this->ruedas = xruedas;
-}
-
-void vehiculo::setPotencia(string xpotencia){
-    this->potencia = xpotencia;
-}
-
-void vehiculo::setColor(string xcolor){
-    this->color = xcolor;
-}
-
-void vehiculo::setAccesorio(string xaccesorio){
-
-
-}
-
-void vehiculo::setCombustible(string xcombustible){
-    this->combustible = xcombustible;
-}
-
-void vehiculo::setBocina(bool xbocina){
-    this->bocina = xbocina;
-
-}
-
-void vehiculo::setPosavasos(bool xportavasos){
-    this->portavasos = xportavasos;
-
-}
-
-void vehiculo::setBurbuja(bool xburbuja){
-    this->burbuja = xburbuja;
-}
-
-void vehiculo::setCocheDeHomer(bool xcocheDeHomer){
-
-}
-
-string vehiculo::toString(){
-    string cadenaVehiculo;
-    cadenaVehiculo = "Tipo de coche: " + this->getTipo() + ", su nombre: " + this->getNombre() + ", su matrícula: " + this->generarMatricula() + "\n";
-    return cadenaVehiculo;
-}
+#endif //VEHICULO_H
